e64c3008bc88073b5ec20fd1d658ed8753912b5e
Sydher <hugoc.pro@gmail.com>
2024-03-18 18:39:53 +0100

    Mise à jours des images é

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -14,10 +14,18 @@ export class PortfolioComponent {
   siteName: string = "M Conception"
 
   projects: any[] = [
-    {id: 0, title: "Projet 1", img: "/assets/images/projects/000.jpg", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ipsum odio, gravida a gravida quis, bibendum nec neque. Integer blandit sollicitudin dolor ut pharetra. Nam vulputate urna et diam venenatis viverra. Nullam non sollicitudin tellus. Praesent in erat a elit elementum mollis sed in diam. Nullam mollis, ligula sed iaculis suscipit, nisl eros pellentesque enim, sed mollis tortor ligula quis turpis. Aenean eu cursus nulla, vel interdum dui."},
-    {id: 1, title: "Projet 2", img: "/assets/images/projects/001.jpg", desc: "Cras gravida placerat pulvinar. Nulla id iaculis enim, id efficitur felis. Ut metus lectus, iaculis id tellus non, rutrum vulputate quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."},
-    {id: 2, title: "Projet 3", img: "/assets/images/projects/002.jpg", desc: "In fringilla mi nec diam elementum, in fringilla orci cursus. Proin porta placerat dui, ut elementum purus ullamcorper a. Etiam elit eros, viverra non diam a, ornare dapibus ligula. Suspendisse vestibulum massa mi. Mauris eget enim odio. Fusce ut ullamcorper turpis. Etiam euismod purus tincidunt gravida lacinia."},
-    {id: 3, title: "Sabre Laser", img: "/assets/images/projects/003.jpg", desc: "Un sabre laser pour tout fan de Star Wars qui se respecte !"},
+    { id: 0, title: "Barrière", img: "/assets/images/projects/barriere.jpeg" },
+    { id: 1, title: "Cache USB", img: "/assets/images/projects/cacheusb.jpeg" },
+    { id: 2, title: "Cadre Ville", img: "/assets/images/projects/cadreville.jpeg" },
+    { id: 3, title: "Cale", img: "/assets/images/projects/cale.jpeg" },
+    { id: 4, title: "Catenaire", img: "/assets/images/projects/catenaire.jpeg" },
+    { id: 5, title: "Circuit Albi", img: "/assets/images/projects/circuit albi.jpeg" },
+    { id: 6, title: "Clavier", img: "/assets/images/projects/clavier.jpeg" },
+    { id: 7, img: "/assets/images/projects/levaigui.jpeg" },
+    { id: 8, title: "Main Support", img: "/assets/images/projects/main.jpeg" },
+    { id: 9, title: "Main Support", img: "/assets/images/projects/main2.jpeg" },
+    { id: 10, title: "Coopération", img: "/assets/images/projects/serragemain.jpeg" },
+    { id: 11, title: "Train", img: "/assets/images/projects/train.jpeg" },
   ]
 
   constructor(private router: Router, private navbarService: NavbarService) {
